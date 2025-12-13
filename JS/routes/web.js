e47e5ee481411e1649b20c58b3ca77b56a8e77b1
NGUYỄN DUY TÂN <duytank321@gmail.com>
2025-12-13 17:48:54 +0700

    Update web.js

--- JS/routes/web.js
+++ JS/routes/web.js
@@ -1,25 +1,16 @@
 const express = require('express');
-<<<<<<< HEAD
 const router = express.Router();
+const authMiddleware = require('../middleware/auth');
+const path = require("path");
 
-// khai báo route
-router.get('/', (req, res) => {
-  res.send('Hello World!');
+// Trang chủ
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-router.get('/', (req, res) => {
-  const port = 3000; // ✅ Khai báo ở đây nếu cần
-  res.send(`Server is running on port ${port}`);
+// Trang riêng tư
+router.get('/dashboard', authMiddleware, (req, res) => {
+    res.render('dashboard.ejs', { user: req.session.user });
 });
 
 module.exports = router;
-=======
-const { getHomepage } = require('../controllers/homeControllers');
-const router = express.Router();
-
-// Route trang chủ
-router.get('/', getHomepage);
-
-
-module.exports = router;
->>>>>>> 5247a51d76e8a77a7e757084e5f84ee459ae7d98
